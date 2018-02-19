3aa58868f1584978b7a47af316bf4c5635cf15ac
Raymond Yin <ryin@seas.upenn.edu>
2018-02-19 15:04:26 -0500

    Blueprint basic frontend

--- webapp/reactapp/frontend/src/App.js
+++ webapp/reactapp/frontend/src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+import { Alignment, Navbar, NavbarDivider, NavbarGroup, NavbarHeading, Button, Search } from "@blueprintjs/core";
+
+import { Card, Elevation } from "@blueprintjs/core";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar>
+          <NavbarGroup>
+            <NavbarHeading>Review Aggregator</NavbarHeading>
+          </NavbarGroup>
+          <NavbarGroup>
+            <Button className="pt-minimal" iconName="home"></Button>
+            
+            <div className="pt-input-group .modifier">
+              <span className="pt-icon pt-icon-search"></span>
+              <input className="pt-input" modifier type="search" placeholder="Search for a product..." dir="auto" />
+            </div>
+            <NavbarDivider />
+            <Button className="pt-minimal" iconName="user"></Button>
+            <Button className="pt-minimal" iconName="notifications"></Button>
+            <Button className="pt-minimal" iconName="cog"></Button>
+          </NavbarGroup>
+        </Navbar>
+
+
+      <Card interactive={true} elevation={Elevation.TWO}>
+          <h5><a href="#">Arctix Men's Essential Snow Pants</a></h5>
+          <p>1,169 customer reviews | 4.0/5 stars</p>
+          <Button>Read More</Button>
+      </Card>
+
+      <Card interactive={true} elevation={Elevation.TWO}>
+          <h5><a href="#">Arctix Men's Essential Snow Pants</a></h5>
+          <p>1,169 customer reviews | 4.0/5 stars</p>
+          <Button>Read More</Button>
+      </Card>
+
+      <Card interactive={true} elevation={Elevation.TWO}>
+          <h5><a href="#">Arctix Men's Essential Snow Pants</a></h5>
+          <p>1,169 customer reviews | 4.0/5 stars</p>
+          <Button>Read More</Button>
+      </Card>
+
       </div>
     );
   }
