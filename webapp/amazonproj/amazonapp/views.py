0eefd9893232151ecfc73a82d175fdffaf1cde04
Raymond Yin <ryin@seas.upenn.edu>
2018-03-28 13:51:39 -0400

    Search API initial pass

--- webapp/amazonproj/amazonapp/views.py
+++ webapp/amazonproj/amazonapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+
 # http://www.django-rest-framework.org/tutorial/quickstart/
 from .models import *
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .serializers import ProductSerializer
 
 
@@ -14,3 +16,15 @@ class ProductViewSet(viewsets.ModelViewSet):
     """
     queryset = Product.objects.all().order_by('title')
     serializer_class = ProductSerializer
+
+    def list(self, request):
+        # filter out those that dont match the search query
+        query = request.GET['q']
+        if query:
+            filtered_products = Product.objects.all().filter(
+                title__startswith=query
+            ).order_by('title')
+        else:
+            filtered_products = self.get_queryset()
+        serializer = self.get_serializer(filtered_products, many=True)
+        return Response(serializer.data)
