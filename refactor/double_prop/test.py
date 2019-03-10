c437a8f0bfdb8132ef75536d6ec7cc386fd89023
Joseph Cappadona <josephcappadona27@gmail.com>
2019-03-10 15:02:22 -0400

    Refactored, modularized

--- refactor/double_prop/test.py
+++ refactor/double_prop/test.py
@@ -1,11 +1,26 @@
+from sys import argv as args
 from corpus import CorpusInfo
 from double_prop import DoublePropagationInfo
+import json
 
-reviews = ['The headphones are great. The headphones are big.', 'The headphones are big and fitting. The headphones and cable are high quality.']
-corpus_info = CorpusInfo(reviews)
-corpus_info.extract_dependency_information()
-dp = DoublePropagationInfo()
-print(list(dp.iterate(corpus_info)))
-print(list(dp.iterate(corpus_info)))
-print(list(dp.iterate(corpus_info)))
+def get_reviews(reviews_filepath):
+    with open(reviews_filepath, 'rt') as reviews_file:
+        for line in reviews_file:
+            yield json.loads(line)
+
+if __name__ == '__main__':
+
+    if len(args) != 2:
+        print('USAGE:  python test.py REVIEWS.json')
+        exit()
+    reviews_filepath = args[1]
+
+    reviews = [review['reviewText'] for review in get_reviews(reviews_filepath)]
+
+    corpus_info = CorpusInfo(reviews)
+    corpus_info.extract_dependency_information()
+    dp = DoublePropagationInfo()
+    print(list(dp.iterate(corpus_info)))
+    print(list(dp.iterate(corpus_info)))
+    print(list(dp.iterate(corpus_info)))
 
