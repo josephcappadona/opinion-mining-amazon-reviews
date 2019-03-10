312e9b4fefed06d97f38c16a9015ac6d79a4ed99
Joseph Cappadona <josephcappadona27@gmail.com>
2019-03-10 14:52:23 -0400

    Modified variable names and usage string

--- refactor/clustering/build_classes.py
+++ refactor/clustering/build_classes.py
@@ -12,11 +12,12 @@ def parse_config(config_filepath):
 
 if __name__ == '__main__':
 
-    if len(args) != 4:
-        print('USAGE:  python build_classes.py CORPUS_FILEPATH.txt OUTPUT_FILEPATH.txt CONFIG_FILEPATH.yaml')
-    corpus_filepath = args[1]
-    output_filepath = args[2]
-    config_filepath = args[3]
+    if len(args) != 5:
+        print('USAGE:  python build_classes.py WORD2VEC_PATH CORPUS.txt OUTPUT.txt W2V_CONFIG.yaml')
+    word2vec_path = args[1]
+    corpus_filepath = args[2]
+    output_filepath = args[3]
+    config_filepath = args[4]
 
     word2vec_args = parse_config(config_filepath)
-    os.system('../../../word2vec/word2vec -train %s -output %s %s' % (corpus_filepath, output_filepath, word2vec_args))
+    os.system('%s -train %s -output %s %s' % (word2vec_path, corpus_filepath, output_filepath, word2vec_args))
