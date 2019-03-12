08177e62a85d00e685a9234fcd0e8bf23246ee0c
Joseph Cappadona <josephcappadona27@gmail.com>
2019-03-12 14:04:49 -0400

    Made refactor directory primary directory

--- nlp/src/clustering/train_classes.sh
+++
@@ -1,3 +0,0 @@
-time ./word2vec/word2vec -train ./results/reviews_data_word2vec.txt -output ./results/classes.txt -cbow 1 -size 300 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -iter 15 -min-count 100 -debug 2 -classes 500
-sort ./results/classes.txt -k 2 -n > ./results/classes.sorted.txt
-echo The word classes were saved to file classes.sorted.txt
--- nlp/src/evaluation/print_top_products.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-with open("../../../../results/top_products.pkl", "rb") as file:
-	asin_buckets = pickle.load(file)
-print(asin_buckets["51-100"])
